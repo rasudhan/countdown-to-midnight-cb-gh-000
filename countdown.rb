ecf6664a24ef135195f447f90cc3c48fe0be665c
Anandha Sudhan <rasudhan@gmail.com>
2018-10-16 23:44:17 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -8,6 +8,11 @@ def countdown(num)
   "HAPPY NEW YEAR!"
 end
 
-def countdown_with_sleep(time)
-  sleep(time)
+def countdown_with_sleep(seconds_to_midnight)
+  while seconds_to_midnight > 0
+   puts "#{seconds_to_midnight} SECOND(S)!"
+   sleep(1)
+   seconds_to_midnight -= 1
+ end
+ "HAPPY NEW YEAR!"
 end
